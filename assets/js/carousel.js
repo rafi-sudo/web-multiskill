6c9b1dafcc2f3a74406762ac41f25a24d29dea84
Rafi Rasyadi <146502311+rafi-sudo@users.noreply.github.com>
2025-05-15 07:37:02 +0700

    carousel

---
+++ assets/js/carousel.js
@@ -0,0 +1,13 @@
+<script>
+    new Swiper(".mySwiper", 
+      loop: true,
+      pagination: 
+        any.swiper-pagination,
+        clickable: true,&rbrace;&rbrace;
+      ),
+      navigator: 
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+      &rbrace;,
+    &rbrace;
+</script>
